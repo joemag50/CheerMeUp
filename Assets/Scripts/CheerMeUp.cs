a0b09310fdcb9d5c90d18619c95f72e71d6d67ab
José Gamboa <j.gamboa@ukko.mx>
2019-01-22 13:15:07 -0600

    Nuevas frases y confetti

--- Assets/Scripts/CheerMeUp.cs
+++ Assets/Scripts/CheerMeUp.cs
@@ -11,7 +11,7 @@ public class CheerMeUp : MonoBehaviour
 	void Start ()
 	{
 		//Load cheers from memory (Let 30 cheers in local)
-		localCheers = new string[10];
+		localCheers = new string[26];
 
 		localCheers[0] = "Eres genial";
 		localCheers[1] = "Tienes muchas cualidades buenas";
@@ -23,6 +23,23 @@ public class CheerMeUp : MonoBehaviour
 		localCheers[7] = "Intenta algo diferente";
 		localCheers[8] = "Retate a ti mismo";
 		localCheers[9] = "Aceptate como eres";
+
+		localCheers[10] = "Quiere a tu familia";
+		localCheers[11] = "La vida en verdad es bella";
+		localCheers[12] = "No estas solo";
+		localCheers[13] = "Piensa antes de hablar";
+		localCheers[14] = "Cuida a tus seres queridos";
+		localCheers[15] = "Respetate, quierete a ti mismo";
+		localCheers[16] = "Cuida tus impulsos";
+		localCheers[17] = "Vive la vida";
+		localCheers[18] = "Eres libre de pensar lo que deseas";
+		localCheers[19] = "Cumple tus metas";
+		localCheers[20] = "Da lo mejor de ti";
+		localCheers[21] = "Da sin esperar a cambio";
+		localCheers[22] = "Muchas personas pasan por lo mismo que tu";
+		localCheers[23] = "Antes de hablar, primero escucha";
+		localCheers[24] = "Conoce tus limites";
+		localCheers[25] = "Visita a tus amigos";
 
 		//Load cheers from server (async from heroku)
 		//Set array of cheers
